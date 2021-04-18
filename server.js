5b83756a54425e6c87bed2a81e6c302c3e16b921
pmin825 <pmin.0825@gmail.com>
2021-04-17 21:18:12 -0700

    implement socket io

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const db = require("./config/keys").mongoURI;
 const app = express();
+const server = require("http").createServer(app);
+const io = require("socket.io")(server);
 const zones = require("./data/zones");
 const mongoose = require("mongoose");
 const zonesRouter = require("./routes/zones");
@@ -19,7 +21,29 @@ mongoose
 app.use("/api/zones", zonesRouter);
 app.use("/api/users", usersRouter);
 
+let users = [];
+io.on("connect", (socket) => {
+  console.log("hello from the server. Socket ID: " + socket.id);
+  users.push(socket.id);
+  io.emit("userList", users);
+  console.log("Users after connection: ", users);
+
+  socket.on("updateUsers", () => {
+    io.emit("userList", users);
+  });
+
+  socket.on("newMessage", (newMessage) => {
+    io.emit("newMessage", newMessage);
+  });
+
+  socket.on("disconnect", () => {
+    users = users.filter((user) => user !== socket.id);
+    io.emit("userList", users);
+    console.log("Users after disconnection:  ", users);
+  });
+});
+
 const port = process.env.PORT || 5000;
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Server is listening on https://localhost:${port}`);
 });
